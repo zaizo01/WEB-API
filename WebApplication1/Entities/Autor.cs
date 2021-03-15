1626113032cdeeb23269183cc0b165c9d98e02e2
Cristopher Zaiz <e.zaizortega@gmail.com>
2021-03-15 19:51:43 -0400

    model validations

--- WebApplication1/Entities/Autor.cs
+++ WebApplication1/Entities/Autor.cs
@@ -10,7 +10,14 @@ namespace WebApplication1.Entities
     {
       public int id { get; set; }
       [Required]
+      [StringLength(20, MinimumLength = 5, ErrorMessage ="El nombre debe de tener {1} caracteres o menos")]
       public string Nombre { get; set; }
       public List<Libro> Libros { get; set; }
+        [Range(18, 120, ErrorMessage = "Solo se permite de 18-120 anos de edad")]
+        public int edad { get; set; }
+        [CreditCard]
+        public string creditCard { get; set; }
+        [Url]
+        public Uri url { get; set; }
     }
 }
---
+++ WebApplication1/Migrations/20210315233331_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "creditCard",
+                table: "Autores",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "url",
+                table: "Autores",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "creditCard",
+                table: "Autores");
+
+            migrationBuilder.DropColumn(
+                name: "url",
+                table: "Autores");
+        }
+    }
+}
